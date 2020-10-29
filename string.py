a0726bfc3c94674ab28b6373fbf89104c9f67257
Ashurosh Acharya <59805898+ashutoshacharya24@users.noreply.github.com>
2020-10-29 16:26:53 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,27 @@
+'''py program to REVERSE content of the given string'''
+
+''' using slice operator  '''
+
+# a = 'ashu'
+# print(a[::-1])
+
+''' using reversed() function'''
+
+# a = 'ashu'
+# r = reversed(a)
+# print(type(r)) 
+# output=''.join(r)
+# print(output)
+
+''' using while loop'''
+
+# s = 'ashutosh'
+# output = ''
+# i = len(s)-1
+# while i >=0:
+#     output = output + s[i]
+#     i = i-1
+# print(output)
+
+
+
